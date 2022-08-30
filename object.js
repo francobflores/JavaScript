1cfa9dc9167ea321657b54181a746b9eddaf1696
Franco Flores <109537044+francobflores@users.noreply.github.com>
2022-08-30 19:05:55 -0300

    :pencil: Objects

---
+++ object.js
@@ -0,0 +1,17 @@
+//Object person
+const person = {
+    nome: "John",
+    age: 19,
+    isAdmin: true
+}
+
+//show object in console
+console.log(person)
+
+//show attribute of object
+console.log(person.nome)
+console.log(person.age)
+console.log(person.isAdmin)
+
+//intepolation
+console.log(`o ${person.nome} tem ${person.age} de idade.`)
