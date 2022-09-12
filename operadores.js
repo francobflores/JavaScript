eeebbee7fae0b20229f67a5d9ed102aacdb74c50
Franco Flores <109537044+francobflores@users.noreply.github.com>
2022-09-12 19:49:49 -0300

    :pencil: ternary operator

---
+++ operadores.js
@@ -0,0 +1,17 @@
+// operator ternary
+
+let pao = true
+let queijo = false
+
+const niceBreakfast = pao && queijo ? 'good coffe!' : 'bad coffee!'
+
+console.log(niceBreakfast)
+
+
+// if driven
+
+let age = 9
+
+const drivein = age >= 18 ? 'drivein' : 'drive out'
+
+console.log(drivein)
