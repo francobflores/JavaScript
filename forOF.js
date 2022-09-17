18ef1096d8bfe1570f576c2c3fb3f3e20c721ea8
Franco Flores <francobarbosaflores@gmail.com>
2022-09-17 20:42:53 -0300

    :pencil:study of for of

---
+++ forOF.js
@@ -0,0 +1,26 @@
+numeros = [ 1, 2, 3, 4, 5 ]
+// print specific index
+console.log(numeros[0])
+
+for(let numero of numeros)
+{
+    console.log(`${numero}`)
+    
+}
+
+// given string in array
+let nomes = ['Franco', 'Bianca', 'Letícia']
+
+for (let nome of nomes)
+{
+    console.log(nome)
+}
+
+//given characteres in String
+
+let palavra = 'Franco'
+
+for(let char of palavra)
+{
+    console.log(char)
+}
